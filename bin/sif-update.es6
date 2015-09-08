fa8d0d2290b0c4fdd28b67c38a3abd7204ea93b4
Volkan Özçelik <volkan@o2js.com>
2015-09-08 09:35:15 -0700

    merge conflict.

--- bin/sif-update.es6
+++ bin/sif-update.es6
@@ -20,10 +20,133 @@
  */
 
 import program from 'commander';
+import byline from 'byline';
+import request from 'request';
+import {join} from 'path';
+import {spawn} from 'child_process';
+import {createReadStream as read, createWriteStream as write} from 'fs';
 
-import {printHeader as header, print, printBlank as blank} from '../lib/terminal/out';
+import {printHeader as header, print, printBlank as blank, printError as error} from '../lib/terminal/out';
+
+const COMMAND = 'update';
+const SUCCESS = 200;
+const INDEX_FILE = join(__dirname, '../data/index.idx');
+const TMP_EXISTING_FILE = join(__dirname, '__tmp_processed');
+const TMP_PROCESSED_FILE = join(__dirname, '__tmp_existing');
 
 program.parse(process.argv);
 
-print('update', 'Command not implemented yet!');
-blank();
+// TODO: this file needs some cleanup.
+
+print(COMMAND, 'Started updating the index… This may take a while. Please be patient…');
+
+let copyAssets = () => {
+    let cat = spawn('cat', [TMP_EXISTING_FILE, TMP_PROCESSED_FILE]);
+    let sort = spawn('sort', ['-u']);
+
+    let indexWriteStream = write(INDEX_FILE, {encoding: 'utf8'});
+
+    sort.stdout.pipe(indexWriteStream);
+
+    cat.stdout.on('data', (line) => sort.stdin.write(line) );
+    cat.stdout.on('end', () => sort.stdin.end() );
+    sort.stdout.on('end', () => indexWriteStream.end() );
+    sort.stdout.on('end', () => print(COMMAND, 'Index file has been successfully updated.') )
+};
+
+let backup = spawn('cp', [INDEX_FILE, INDEX_FILE + '.backup']);
+
+backup.stdout.on('end', () => {
+    let inStream = byline(read(INDEX_FILE, {encoding: 'utf8'}));
+
+    let tmpExistingFileWriteStream = write(TMP_EXISTING_FILE, {encoding: 'utf8'});
+    let tmpProcessedFileWriteStream = write(TMP_PROCESSED_FILE, {encoding: 'utf8'});
+
+    // can be done with promises too.
+    let remainingMetaDataRequests = 0;
+    let inStreamEnded = false;
+
+    let maybeEndStreamsThenCopyAssets = null;
+
+    maybeEndStreamsThenCopyAssets = () => {
+        if (inStreamEnded && remainingMetaDataRequests === 0) {
+            let maybeCopyAssets = null;
+            let counter = 2;
+
+            maybeCopyAssets = () => {
+                counter--;
+
+                if (counter === 0) {
+                    copyAssets();
+                }
+
+                maybeCopyAssets = () => {};
+            };
+
+            // TODO: this part "begs" for promises.
+            tmpProcessedFileWriteStream.on('finish', maybeCopyAssets);
+            tmpExistingFileWriteStream.on('finish', maybeCopyAssets);
+
+            tmpProcessedFileWriteStream.end();
+            tmpExistingFileWriteStream.end();
+
+            maybeEndStreamsThenCopyAssets = () => {};
+        }
+    };
+
+    inStream.on('data', (line) => {
+
+        // TODO: to a util library function.
+        let occurrences = line.split('<::sif::>').length - 1;
+        let needsProcessing = occurrences === 0;
+        let alreadyProcessed = occurrences === 1;
+        let malformed = !needsProcessing && !alreadyProcessed;
+
+        if (malformed) {
+            error(COMMAND, `badly-formatted line: "${line.replace(/sif/g, '__sif__')}"`);
+
+            return;
+        }
+
+        if (needsProcessing) {
+            remainingMetaDataRequests++;
+
+            let url = line.trim();
+
+            request(url, (error, response, body) => {
+                remainingMetaDataRequests--;
+
+                if (error || response.statusCode !== SUCCESS) {
+                    maybeEndStreamsThenCopyAssets();
+
+                    return;
+                }
+
+                let replaced = body.replace(/\s+/g, ' ');
+                let result = /<title>(.*?)<\/title>/i.exec(replaced);
+                let title = result[1];
+
+                if (title) {
+                    tmpProcessedFileWriteStream.write(url + ' <::sif::> ' + title + '\n');
+                } else {
+                    error(COMMAND, `no title found for: "${url}"; I'll leave it untouched. — Please file a bug at xxx to get it fixed.`);
+
+                    tmpExistingFileWriteStream.write(url + '\n');
+                }
+
+                maybeEndStreamsThenCopyAssets();
+            });
+
+            return;
+        }
+
+        tmpExistingFileWriteStream.write(line.trim() + '\n');
+    });
+
+    inStream.on('end', () => {
+        inStreamEnded = true;
+
+        // on `end`, all the data is consumed.
+        maybeEndStreamsThenCopyAssets();
+    });
+});
--- bin/sif-update.js
+++ bin/sif-update.js
@@ -26,11 +26,159 @@ var _commander = require('commander');
 
 var _commander2 = _interopRequireDefault(_commander);
 
+var _byline = require('byline');
+
+var _byline2 = _interopRequireDefault(_byline);
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _path = require('path');
+
+var _child_process = require('child_process');
+
+var _fs = require('fs');
+
 var _libTerminalOut = require('../lib/terminal/out');
+
+var COMMAND = 'update';
+var SUCCESS = 200;
+var INDEX_FILE = (0, _path.join)(__dirname, '../data/index.idx');
+var TMP_EXISTING_FILE = (0, _path.join)(__dirname, '__tmp_processed');
+var TMP_PROCESSED_FILE = (0, _path.join)(__dirname, '__tmp_existing');
 
 _commander2['default'].parse(process.argv);
 
-(0, _libTerminalOut.print)('update', 'Command not implemented yet!');
-(0, _libTerminalOut.printBlank)();
+// TODO: this file needs some cleanup.
+
+(0, _libTerminalOut.print)(COMMAND, 'Started updating the index… This may take a while. Please be patient…');
+
+var copyAssets = function copyAssets() {
+    var cat = (0, _child_process.spawn)('cat', [TMP_EXISTING_FILE, TMP_PROCESSED_FILE]);
+    var sort = (0, _child_process.spawn)('sort', ['-u']);
+
+    var indexWriteStream = (0, _fs.createWriteStream)(INDEX_FILE, { encoding: 'utf8' });
+
+    sort.stdout.pipe(indexWriteStream);
+
+    cat.stdout.on('data', function (line) {
+        return sort.stdin.write(line);
+    });
+    cat.stdout.on('end', function () {
+        return sort.stdin.end();
+    });
+    sort.stdout.on('end', function () {
+        return indexWriteStream.end();
+    });
+    sort.stdout.on('end', function () {
+        return (0, _libTerminalOut.print)(COMMAND, 'Index file has been successfully updated.');
+    });
+};
+
+var backup = (0, _child_process.spawn)('cp', [INDEX_FILE, INDEX_FILE + '.backup']);
+
+backup.stdout.on('end', function () {
+    var inStream = (0, _byline2['default'])((0, _fs.createReadStream)(INDEX_FILE, { encoding: 'utf8' }));
+
+    var tmpExistingFileWriteStream = (0, _fs.createWriteStream)(TMP_EXISTING_FILE, { encoding: 'utf8' });
+    var tmpProcessedFileWriteStream = (0, _fs.createWriteStream)(TMP_PROCESSED_FILE, { encoding: 'utf8' });
+
+    // can be done with promises too.
+    var remainingMetaDataRequests = 0;
+    var inStreamEnded = false;
+
+    var maybeEndStreamsThenCopyAssets = null;
+
+    maybeEndStreamsThenCopyAssets = function () {
+        if (inStreamEnded && remainingMetaDataRequests === 0) {
+            (function () {
+                var maybeCopyAssets = null;
+                var counter = 2;
+
+                maybeCopyAssets = function () {
+                    counter--;
+
+                    if (counter === 0) {
+                        copyAssets();
+                    }
+
+                    maybeCopyAssets = function () {};
+                };
+
+                // TODO: this part "begs" for promises.
+                tmpProcessedFileWriteStream.on('finish', maybeCopyAssets);
+                tmpExistingFileWriteStream.on('finish', maybeCopyAssets);
+
+                tmpProcessedFileWriteStream.end();
+                tmpExistingFileWriteStream.end();
+
+                maybeEndStreamsThenCopyAssets = function () {};
+            })();
+        }
+    };
+
+    inStream.on('data', function (line) {
+
+        // TODO: to a util library function.
+        var occurrences = line.split('<::sif::>').length - 1;
+        var needsProcessing = occurrences === 0;
+        var alreadyProcessed = occurrences === 1;
+        var malformed = !needsProcessing && !alreadyProcessed;
+
+        if (malformed) {
+            (0, _libTerminalOut.printError)(COMMAND, 'badly-formatted line: "' + line.replace(/sif/g, '__sif__') + '"');
+
+            return;
+        }
+
+        if (needsProcessing) {
+            var _ret2 = (function () {
+                remainingMetaDataRequests++;
+
+                var url = line.trim();
+
+                (0, _request2['default'])(url, function (error, response, body) {
+                    remainingMetaDataRequests--;
+
+                    if (error || response.statusCode !== SUCCESS) {
+                        maybeEndStreamsThenCopyAssets();
+
+                        return;
+                    }
+
+                    var replaced = body.replace(/\s+/g, ' ');
+                    var result = /<title>(.*?)<\/title>/i.exec(replaced);
+                    var title = result[1];
+
+                    if (title) {
+                        tmpProcessedFileWriteStream.write(url + ' <::sif::> ' + title + '\n');
+                    } else {
+                        error(COMMAND, 'no title found for: "' + url + '"; I\'ll leave it untouched. — Please file a bug at xxx to get it fixed.');
+
+                        tmpExistingFileWriteStream.write(url + '\n');
+                    }
+
+                    maybeEndStreamsThenCopyAssets();
+                });
+
+                return {
+                    v: undefined
+                };
+            })();
+
+            if (typeof _ret2 === 'object') return _ret2.v;
+        }
+
+        tmpExistingFileWriteStream.write(line.trim() + '\n');
+    });
+
+    inStream.on('end', function () {
+        inStreamEnded = true;
+
+        // on `end`, all the data is consumed.
+        maybeEndStreamsThenCopyAssets();
+    });
+});
 
 //# sourceMappingURL=sif-update.js.map
